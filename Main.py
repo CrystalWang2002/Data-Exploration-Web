f2a4007ec3e5f66ad281063dcb17fda40e344562
CrystalWang2002 <147997879+CrystalWang2002@users.noreply.github.com>
2023-12-31 16:22:29 +0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("Main Page")
+
+st.write("""
+🚀 Welcome to my personal webpage, where I share my passion for data! 🚀
+         
+🔍Here, you'll find my unique insights into intriguing datasets, showcasing my skills in data processing and visualization. 
+
+📈I also offer my perspectives on business analytics, blending data-driven analysis with strategic thinking. 
+
+📝Currently, my website features two main projects: 'Amazon Sales Analysis' and 'Bank Customer Analysis'. 
+In the 'Amazon Sales Analysis', I delve into the vast world of online retail, exploring sales trends, customer behaviors, and market dynamics. 
+The 'Bank Customer Analysis' project focuses on the banking sector, where I analyze customer data to uncover patterns in financial behaviors and preferences. 
+         
+📚My journey doesn't stop here, I am committed to continuously updating my site with new and valuable datasets. Stay tuned as I dive deeper into the data, offering profound business insights and uncovering hidden stories in the numbers!
+
+✨Join me in exploring the fascinating world of data and its impact on the business landscape!🌐✨
+
+""")
